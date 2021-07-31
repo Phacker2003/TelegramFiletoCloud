05450d74cc0428d464feddf2f13f416d09ec2e00
Phacker2003 <83575931+Phacker2003@users.noreply.github.com>
2021-07-31 10:21:22 +0530

    Update

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-# This is bot coded by AbhijithCloud and used for educational purposes only
-# https://github.com/AbhijithNT
-# Copyright ABHIJITH N T
-# Thank you https://github.com/pyrogram/pyrogram
+# BOT BY @PARVSHAH_01 
+# JUST SEND ME A FILE I WILL UPLOAD IT TO CLOUD
 
 import pyrogram
 from bot.filetocloud import CloudBot
